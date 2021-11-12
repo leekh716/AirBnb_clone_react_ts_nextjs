0aa2d30f9ea1a45a8f67a468540f94bb550c0922
kunlee <leekh716@naver.com>
2021-11-12 20:09:51 +0900

    bnb signup ui

--- next-bnb/components/auth/SignUpModal.tsx
+++ next-bnb/components/auth/SignUpModal.tsx
@@ -5,7 +5,11 @@ import MailIcon from "../../public/static/svg/auth/mail.svg";
 import PersonIcon from "../../public/static/svg/auth/person.svg";
 import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
 import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
-import Input from "../common/input";
+import Input from "../common/Input";
+import Selector from "../common/Selector";
+import { dayList, monthList, yearList } from "../../lib/staticData";
+import palette from "../../styles/palette";
+import Button from "../common/Button";
 
 const Container = styled.div`
 	width: 568px;
@@ -24,6 +28,38 @@ const Container = styled.div`
 		position: relative;
 		margin-bottom: 16px;
 	}
+
+	.sign-up-birthday-label {
+		font-size: 15px;
+		font-weight: 600;
+		margin-top: 16px;
+		margin-bottom: 8px;
+	}
+	.sign-up-modal-birthday-info {
+		margin-bottom: 16px;
+		color: ${palette.charcoal};
+	}
+
+	.sign-up-modal-birthday-selectors {
+		display: flex;
+		margin-bottom: 24px;
+		.sign-up-modal-birthday-month-selector {
+			margin-right: 16px;
+			flex-grow: 1;
+		}
+		.sign-up-modal-birthday-day-selector {
+			margin-right: 16px;
+			width: 25%;
+		}
+		.sign-up-modal-birthday-year-selector {
+			width: 33.3333%;
+		}
+	}
+	.sign-up-modal-submit-button-wrapper {
+		margin-bottom: 16px;
+		padding-bottom: 16px;
+		border-bottom: 1px solid ${palette.gray_eb};
+	}
 `;
 
 const SignUpModal: React.FC = () => {
@@ -32,6 +68,9 @@ const SignUpModal: React.FC = () => {
 	const [firstname, setFirstname] = useState("");
 	const [password, setPassword] = useState("");
 	const [hidePassword, setHidePassword] = useState(true);
+	const [birthYear, setBirthYear] = useState<string | undefined>();
+	const [birthDay, setBirthDay] = useState<string | undefined>();
+	const [birthMonth, setBirthMonth] = useState<string | undefined>();
 
 	const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setEmail(event.target.value);
@@ -49,6 +88,16 @@ const SignUpModal: React.FC = () => {
 	const toggleHidePassword = () => {
 		setHidePassword(!hidePassword);
 	};
+
+	const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setBirthMonth(event.target.value);
+	};
+	const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setBirthDay(event.target.value);
+	};
+	const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		setBirthYear(event.target.value);
+	}
 
 	return (
 		<Container>
@@ -76,6 +125,25 @@ const SignUpModal: React.FC = () => {
 					value={password}
 					onChange={onChangePassword}
 				/>
+			</div>
+			<p className="sign-up-birthday-label">생일</p>
+			<p className="sign-up-modal-birthday-info">
+				만 18세 이상의 성인만 회원으로 가입할 수 있습니다.
+				생일은 다른 에어비앤비 이용자에게 공개되지 않습니다.
+			</p>
+			<div className="sign-up-modal-birthday-selectors">
+				<div className="sign-up-modal-birthday-month-selector">
+					<Selector options={monthList} disabledOptions={["월"]} defaultValue="월" value={birthMonth} onChange={onChangeBirthMonth} />
+				</div>
+				<div className="sign-up-modal-birthday-day-selector">
+					<Selector options={dayList} disabledOptions={["일"]} defaultValue="일" value={birthDay} onChange={onChangeBirthDay}/>
+				</div>
+				<div className="sign-up-modal-birthday-year-selector">
+					<Selector options={yearList} disabledOptions={["년"]} defaultValue="년" value={birthYear} onChange={onChangeBirthYear}/>
+				</div>
+			</div>
+			<div className="sign-up-modal-submit-button-wrapper">
+				<Button type="submit">가입하기</Button>
 			</div>
 		</Container>
 	);
---
+++ next-bnb/lib/staticData.ts
@@ -0,0 +1,18 @@
+export const monthList = [
+	"1월",
+	"2월",
+	"3월",
+	"4월",
+	"5월",
+	"6월",
+	"7월",
+	"8월",
+	"9월",
+	"10월",
+	"11월",
+	"12월",
+];
+
+export const dayList = Array.from(Array(31), (v, i) => String(i + 1));
+
+export const yearList = Array.from(Array(122), (v, i) => String(2021 - i));
