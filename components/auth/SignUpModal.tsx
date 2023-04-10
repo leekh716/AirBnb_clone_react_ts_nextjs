e183beaf75618746b5122b1defab6eaf8e0cf578
kunlee <khlee@ebrain.kr>
2023-04-11 01:13:34 +0900

    feat: 회원가입 모달 UI

--- components/auth/SignUpModal.tsx
+++ components/auth/SignUpModal.tsx
@@ -1,14 +1,197 @@
 import styled from "styled-components";
+import React, { useState } from "react";
+import CloseXIcon from "../../public/static/svg/modal/modal_colose_x_icon.svg";
+import MailIcon from "../../public/static/svg/auth/mail.svg";
+import PersonIcon from "../../public/static/svg/auth/person.svg";
+import OpenedEyeIcon from "../../public/static/svg/auth/opened_eye.svg";
+import ClosedEyeIcon from "../../public/static/svg/auth/closed_eye.svg";
+import Input from "../common/Input";
+import palette from "../../styles/palette";
+import Selector from "../common/Selector";
+import { dayList, monthList, yearList } from "../../lib/staticData";
+import Button from "../common/Button";
 
-const Container = styled.div`
+const Container = styled.form`
   width: 568px;
   height: 614px;
+  padding: 32px;
   background-color: white;
   z-index: 11;
+
+  .modal-close-x-icon {
+    cursor: pointer;
+    display: block;
+    margin: 0 0 40px auto;
+  }
+
+  .input-wrapper {
+    position: relative;
+    margin-bottom: 16px;
+  }
+  .sign-up-password-input-wrapper {
+    svg {
+      cursor: pointer;
+    }
+  }
+  .sign-up-birthday-label {
+    font-size: 15px;
+    font-weight: 600;
+    margin-top: 16px;
+    margin-bottom: 8px;
+  }
+  .sign-up-modal-birthday-info {
+    margin-bottom: 16px;
+    color: ${palette.charcoal};
+  }
+
+  .sign-up-modal-birthday-selectors {
+    display: flex;
+    margin-bottom: 24px;
+    .sign-up-modal-birthday-month-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-day-selector {
+      margin-right: 16px;
+      width: 25%;
+    }
+    .sign-up-modal-birthday-year-selector {
+      width: 33.3333%;
+    }
+  }
+  .sign-up-modal-submit-button-wrapper {
+    margin-bottom: 16px;
+    padding-bottom: 16px;
+    border-bottom: 1px solid ${palette.gray_eb};
+  }
 `;
 
-function SignUpModal() {
-  return <Container>Sign Up</Container>;
+interface IProps {
+  closeModal: () => void;
+}
+
+function SignUpModal({ closeModal }: IProps) {
+  const [email, setEmail] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [password, setPassword] = useState("");
+  const [hidePassword, setHidePassword] = useState(true);
+  const [birthYear, setBirthYear] = useState<string | undefined>();
+  const [birthDay, setBirthDay] = useState<string | undefined>();
+  const [birthMonth, setBirthMonth] = useState<string | undefined>();
+
+  const onChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+  const onChangeLastname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setLastname(event.target.value);
+  };
+  const onChangeFirstname = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstname(event.target.value);
+  };
+  const onChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+  const toggleHidePassword = () => {
+    setHidePassword(!hidePassword);
+  };
+  const onChangeBirthMonth = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthMonth(event.target.value);
+  };
+  const onChangeBirthDay = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthDay(event.target.value);
+  };
+  const onChangeBirthYear = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setBirthYear(event.target.value);
+  };
+
+  const disabledMonths = ["월"];
+  const disabledDays = ["일"];
+  const disabledYears = ["년"];
+
+  return (
+    <Container>
+      <CloseXIcon className="modal-close-x-icon" onClick={closeModal} />
+      <div className="input-wrapper">
+        <Input
+          placeholder="이메일 주소"
+          type="email"
+          icon={<MailIcon />}
+          name="email"
+          value={email}
+          onChange={onChangeEmail}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="이름(ex: 길동)"
+          icon={<PersonIcon />}
+          value={lastname}
+          onChange={onChangeLastname}
+        />
+      </div>
+      <div className="input-wrapper">
+        <Input
+          placeholder="성(ex: 홍)"
+          icon={<PersonIcon />}
+          value={firstname}
+          onChange={onChangeFirstname}
+        />
+      </div>
+      <div className="input-wrapper sign-up-password-input-wrapper">
+        <Input
+          placeholder="비밀번호 설정하기"
+          type={hidePassword ? "password" : "text"}
+          icon={
+            hidePassword ? (
+              <ClosedEyeIcon onClick={toggleHidePassword} />
+            ) : (
+              <OpenedEyeIcon onClick={toggleHidePassword} />
+            )
+          }
+          value={password}
+          onChange={onChangePassword}
+        />
+      </div>
+      <p className="sign-up-birthday-label">생일</p>
+      <p className="sign-up-modal-birthday-info">
+        만 18세 이상의 성인만 회원으로 가입할 수 있습니다. 생일은 다른
+        에어비앤비 이용자에게 공개되지 않습니다.
+      </p>
+      <div className="sign-up-modal-birthday-selectors">
+        <div className="sign-up-modal-birthday-month-selector">
+          <Selector
+            options={monthList}
+            disabledOptions={disabledMonths}
+            defaultValue="월"
+            value={birthMonth}
+            onChange={onChangeBirthMonth}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-day-selector">
+          <Selector
+            options={dayList}
+            disabledOptions={disabledDays}
+            defaultValue="일"
+            value={birthDay}
+            onChange={onChangeBirthDay}
+          />
+        </div>
+        <div className="sign-up-modal-birthday-year-selector">
+          <Selector
+            options={yearList}
+            disabledOptions={disabledYears}
+            defaultValue="년"
+            value={birthYear}
+            onChange={onChangeBirthYear}
+          />
+        </div>
+      </div>
+      <div className="sign-up-modal-submit-button-wrapper">
+        <Button type="submit">가입하기</Button>
+      </div>
+    </Container>
+  );
 }
 
 export default SignUpModal;
